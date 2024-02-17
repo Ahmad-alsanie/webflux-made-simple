8aa94250701adfe939a36c4d7aa464b7c4074f8b
Ahmad-alsanie <ahmad.alsanie@hotmail.com>
2024-02-17 11:23:36 +0100

    update: webflux security

---
+++ src/main/java/com/sanie/configuration/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.sanie.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebFluxSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated() // require authentication for any request
+                )
+                .httpBasic(withDefaults()) // enable HTTP Basic authentication
+                .csrf().disable(); // disable CSRF in the case of a RESTful application
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+}
